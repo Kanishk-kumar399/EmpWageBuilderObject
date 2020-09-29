b64b0b5b6171c9383f7076c8a596ed705ca8593e
Kanishk-kumar399 <kanishkk301198@gmail.com>
2020-09-29 21:22:53 +0530

    Added IfComputeEmpWage in UC11branch for interface file

---
+++ IfComputeEmpWage.java
@@ -0,0 +1,6 @@
+public interface IfComputeEmpWage
+{
+	public void addCompanyEmpWage(String company,int empRatePerHour,int numOfWorkDays,int maxHoursMonth);
+	public void computeEmpWage();
+	public int getTotalWage(String company);
+}
